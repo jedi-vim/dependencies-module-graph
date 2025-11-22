392ee0ece6413ac8f43a4030077edbc2be92f7ee
Leonam Silva <leonam.silva.cc@gmail.com>
2025-11-22 18:31:19 -0300

    feat: remover codigo ja copiado para outro local

--- init.lua
+++ init.lua
@@ -5,29 +5,9 @@ local split = require "split"
 local dj = require "django"
 
 -- Inicializar esse modulo passando esse valor
-local DJANGO_PROJECT_ROOT = "/home/leonam/workspace/cotabest"
-
-function compute_total_out(app_tbl)
-  local total = 0
-  for _, count in pairs(app_tbl) do
-    total = total + count
-  end
-  return total
 end
 
-function compute_total_in(apps_tbl)
-  local total_in_tbl = {}
-  for app_name, _ in pairs(apps_tbl) do
-    local total_in = 0
-    for _, data in pairs(apps_tbl) do
-      local t = data[app_name]
-      if t ~= nil then
-        total_in = total_in + data[app_name]
-      end
-    end
-    total_in_tbl[app_name] = total_in
   end
-  return total_in_tbl
 end
 
 local django_apps = dj.find_django_apps(DJANGO_PROJECT_ROOT)
